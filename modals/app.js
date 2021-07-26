99c35df865037fde88b0a3e47b304c50f25b0151
Richard Aspinall <rls.aspinall@gmail.com>
2021-07-27 09:45:03 +1000

    Added signing secrets to modals app

--- modals/app.js
+++ modals/app.js
@@ -1,15 +1,26 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-const express = require('../node_modules/express');
+const express = require('express');
+
+const verifySignature = require('../utils/verify-signature.js');
+
 const modalController = require('./modal-controller');
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Routing interactivity to controller
-app.use('/slack/events', modalController);
+// Verify signature in middleware for urlencoded or json requests
+app.use(express.urlencoded({ extended: true, verify: verifySignature }));
+app.use(express.json({ verify: verifySignature }));
 
+// Verify challenge or send request to controller
+app.use('/slack/events', (req, res) => {
+  // Verify signature
+  if (!req.valid) {
+    return res.sendStatus(404);
+  }
+
+  modalController(req, res);
+});
 app.listen(3000, () => {
   console.log('Server has started');
 });
