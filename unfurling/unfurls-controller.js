cbac5b5744bfd81fe2a762fff6eb153d4b754645
Richard Aspinall <raspinall@slack-corp.com>
2021-09-22 15:28:02 +1000

    Added composer logic

--- unfurling/unfurls-controller.js
+++ unfurling/unfurls-controller.js
@@ -12,11 +12,32 @@ router.post('/', async (req, res) => {
   const channel = req.body.event.channel;
   const ts = req.body.event.message_ts;
 
-  // Call remote.add
-  if (req.body.event.links[0].url === RESOURCE_URL) {
+  // Unfurls when the link is posted into the message input
+  // As per: https://api.slack.com/methods/chat.unfurl#responses
+  if (req.body.event.source == 'composer') {
+    slack.web.chatUnfurl(ts, channel, {
+      [RESOURCE_URL]: {
+        blocks: [
+          {
+            type: 'file',
+            external_id: 'ABC123456789',
+            source: 'remote',
+          },
+        ],
+        preview: {
+          title: {
+            type: 'plain_text',
+            text: 'custom preview displayed here',
+          },
+          icon_url:
+            'https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        },
+      },
+    });
+  } else if (req.body.event.links[0].url === RESOURCE_URL) {
     try {
       const response = await slack.web.filesRemoteAdd(
-        path.join(__dirname, '../files/slack.jpg'),
+        path.join(__dirname, '../unfurling/files/slack.jpg'),
         'Slack Logo',
         RESOURCE_URL,
         'ABC123456789'
