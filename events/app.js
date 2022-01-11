f032fa9ebbbf0a4233ead3ab18d87ec0611aaf0d
Richard Aspinall <raspinall@slack-corp.com>
2022-01-11 14:08:48 +1100

    Tidied up events files

--- events/app.js
+++ events/app.js
@@ -2,9 +2,9 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
 
+const slack = require('../libs/slack_interface/index');
 const verifySignature = require('../utils/verify-signature.js');
-
-const eventsController = require('./events-controller');
+const appHomeView = require('./home.json');
 
 const app = express();
 
@@ -17,15 +17,39 @@ app.use('/slack/events', (req, res) => {
   if (!req.valid) {
     return res.sendStatus(404);
   }
+  // A challenge param is sent to this endpoint when enabling events
   if (req.body.challenge) {
     return res.send({ challenge: req.body.challenge });
   }
-  eventsController(req, res);
+
+  res.sendStatus(200);
+
+  // Check body for event type and log relevant information, catch unhandled events
+  if (req.body.event) {
+    const event = req.body.event.type;
+    switch (event) {
+      case 'link_shared':
+        console.log(req.body.event.links);
+        break;
+      case 'app_mention':
+        console.log(req.body.event);
+        break;
+      case 'app_home_opened':
+        console.log(req.body.event);
+        slack.web.viewsPublish(req.body.event.user, appHomeView);
+        break;
+      case 'team_join':
+        console.log(req.body.event);
+        console.log(req.body.event.user.profile);
+        break;
+      default:
+        console.log(`------------------\nEvent not handled:\n------------------`);
+        console.log(req.body);
+        console.log(`------------------`);
+    }
+  }
 });
 
-app.post('/', (req, res) => {
-  res.sendStatus(200);
-});
 // Start server
 app.listen(3000, () => {
   console.log('Server has started');
